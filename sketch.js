82bcbd6eb4ddf15fd270fce701a03797188aa336
Mohamed Shafee <mshafee1993@gmail.com>
2020-11-10 11:19:51 -0500

    delete graphics utilities class and move functions to sketch.js

--- sketch.js
+++ sketch.js
@@ -20,6 +20,27 @@ let playAgainBTN;
 let betEvenBTN;
 let betOddBTN;
 
+
+// Draw a rect panel with rounded borders
+function bgPanel(x = 0, y = 0, panelWidth = 10, panelHeight = 10, pColor = color(255, 255,255)) {
+	push()
+	fill(pColor);
+	rect(x, y, panelWidth, panelHeight, 20);
+	pop()
+}
+
+// Draw background
+function drawBackground(){
+	push()
+	noFill();
+	stroke(240, 240, 240);
+	strokeWeight(20);
+	image(bgImage, 0 , 0);
+	rect(0,0, width, height, 20);
+	pop();
+}
+
+
 //Load images before the game starts
 function preload() {
 	bgImage = loadImage("./Assets/CaisonBG.jpg");
